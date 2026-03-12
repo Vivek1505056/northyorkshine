4bdf5a427303c728cf74e88656aa9e4ddf590920
Vivek Koul <vivekkoul@Viveks-Laptop.local>
2026-03-11 21:09:43 -0400

    work please

--- src/components/ui/SectionWrapper.jsx
+++
@@ -1,9 +0,0 @@
-export default function SectionWrapper({ id, className = '', children, innerClassName = '' }) {
-  return (
-    <section id={id} className={`section-padding ${className}`}>
-      <div className={`container-width ${innerClassName}`}>
-        {children}
-      </div>
-    </section>
-  )
-}
